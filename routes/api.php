991560ad317c30e590ef37847b8e5039bcd7b0a8
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-08-09 21:59:46 -0300

    Londrina:09.08.2021_21:59hs

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', function () {
-    return ['Chegamos até aqui' => 'SIM'];
-});
+//Mapeamento automático da rota, para todos os métodos do controlador que foi criado
+//com "resource", que já cria todos os métodos padrão.
+//Usando "resource" já criam index, store, create, show, update, destroy e edit no controller.
+/* Route::resource('cliente'  'App\Http\Controllers\ClienteController'); */
+
+//Usando "apiResource" só criam index, store, show, update, e destroy no controller.
+//Para "api", "create" e "edit" não tem necessidade(eles criam um formulários).
+Route::apiResource('cliente', 'ClienteController');
+
+Route::apiResource('carro', 'CarroController');
+Route::apiResource('locacao', 'LocacaoController');
+Route::apiResource('marca', 'MarcaController');
+Route::apiResource('modelo', 'ModeloController');
