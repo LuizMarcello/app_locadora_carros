b60dc2df9b124ce6f364e2f4e4cc16cedd156674
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-10-02 11:55:16 -0300

    Londrina:02.10.2021_11:55hs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,8 +7,27 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
+        //autenticação (email e senha)
+        //Método "all()" retorna um array.
+        $credenciais = $request->all(['email', 'password']);
+
+        //retornar um Json Web Token:
+        //Para autenticar: Método/helper "auth()" do laravel
+        //Método "attempt()": Faz uma tentativa de autenticação
+        //e se der certo, ele retorna um "token"(api).
+        $token = auth('api')->attempt($credenciais);
+        /* dd($token); */
+        if ($token) { //Usuário autenticado com sucesso,
+            return response()->json(['token' => $token]);
+        } else { //Êrro de usuário ou senha
+            return response()->json(['erro' => 'Usuário ou senha incorretos!'], 403);
+            //401 = Unauthorized -> não autorizado
+            //403 = forbidden -> proibido (login inválido)
+        }
+
+
         return 'login';
     }
 
